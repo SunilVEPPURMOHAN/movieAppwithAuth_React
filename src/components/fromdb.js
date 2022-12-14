68572fe449a363806a0b8c4db07e2404ec3bfdab
Sunil Mohan <sunilvm2017@email.iimcal.ac.in>
2022-12-14 22:02:02 +0530

    asdasd

--- src/components/fromdb.js
+++ src/components/fromdb.js
@@ -1,7 +1,7 @@
 
 const movies = require("./moviedb.json");
 
-const url1 = 'http://localhost:8080';
+// const url1 = 'http://localhost:8080';
 const url2 = 'https://dull-cyan-tadpole-yoke.cyclic.app'
 
 module.exports.urla = url2 ;
